fbfe6286df6d20298e9266f41afbffb5f21bbdee
ann <anasr@student.42abudhabi.ae>
2022-11-20 18:51:05 +0400

    Write a script to delete docker volumes

---
+++ srcs/requirements/tools/removeVolumes.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+FRONTEND_VOLUME="requirements_www-duck-data"
+BACKEND_VOLUME="requirements_db-duck-data"
+
+docker volume ls | grep $FRONTEND_VOLUME &> /dev/null
+if [[ $? -eq 0 ]]; then
+	echo "Removed volume $FRONTEND_VOLUME"
+	docker volume rm -f $FRONTEND_VOLUME &> /dev/null
+fi
+
+docker volume ls | grep $BACKEND_VOLUME &> /dev/null
+if [[ $? -eq 0 ]]; then
+	echo "Removed volume '$BACKEND_VOLUME'"
+	docker volume rm -f $BACKEND_VOLUME &> /dev/null
+fi
