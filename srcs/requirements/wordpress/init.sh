906591ee35535c5bb2e01dce33d66c162683d2da
ann <anasr@student.42abudhabi.ae>
2022-11-23 11:19:15 +0400

    Install wordpress, connect database in mariadb server, and create 2 users

--- srcs/requirements/wordpress/init.sh
+++ srcs/requirements/wordpress/init.sh
@@ -1,18 +1,5 @@
 	#!/bin/bash
-	DBNAME=$1
-	DBPASS=$2
-	DBUSER=$3
-	DBHOST=$4
-
-	ADMIN_USER='superPowerRangerr'
-	ADMIN_PASS='2KwecuyX#dCS!Ye4or'
-	ADMIN_EMAIL='hehe@gmail.com'
-
-	NORMAL_USER='anasrr'
-	NORMAL_PASS='blahblah'
-	NORMAL_EMAIL='hehe_normal@gmail.com'
-
-	TITLE='The Depressed French Philosopher Jean-Paul Sartre'
+	TITLE='THIS IS MY SAMPLE WEBSITE.. sorry capslock was on'
 
 #	Move to the proper work directory	
 	cd /var/www/html
@@ -20,23 +7,17 @@
 #	Creating the php config file if does not exist
 	ls | grep "wp-config.php" &> /dev/null
 	if [[ $? -ne 0 ]]; then
-		wp config create --allow-root --dbname=$DBNAME --dbuser=$DBUSER --dbhost=$DBHOST --dbpass=$DBPASS 
-	fi
-
-#	Creating the wordpress database if it does not exist
-	wp db check --allow-root &> /dev/null
-	if [[ $? -ne 0 ]]; then
-		wp db create --allow-root
+		wp config create --allow-root --dbname=$WP_DBNAME --dbuser=$WP_DBUSER --dbhost=$WP_DBHOST --dbpass=$WP_DBPASS 
 	fi
 
 #	Wordpress installation and creation of the admin user
-	wp core install --allow-root --url=anasr --title="$TITLE" --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASS --admin_email=$ADMIN_EMAIL
+	wp core is-installed --allow-root
+	if [[ $? -ne 0 ]]; then
+		wp core install --allow-root --url=anasr --title="$TITLE" --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASS --admin_email=$ADMIN_EMAIL
+	fi
 
-#	Create another user with a subscriber role
+#	Create another user with a default subscriber role
 	wp user create --allow-root $NORMAL_USER $NORMAL_EMAIL --user_pass=$NORMAL_PASS
-	# --role='Subscriber'
 
 #	Update all plugins
 	wp plugin update --all --allow-root
-	
-	echo "Done initailizing WordPress!"
