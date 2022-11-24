5cafad21661b112db8b3c87d8ded58bc5d4a997e
ann <anasr@student.42abudhabi.ae>
2022-11-24 08:19:27 +0400

    Update the init.sh script of wordpress container to allow for media uploads

--- srcs/requirements/wordpress/init.sh
+++ srcs/requirements/wordpress/init.sh
@@ -13,11 +13,28 @@
 #	Wordpress installation and creation of the admin user
 	wp core is-installed --allow-root
 	if [[ $? -ne 0 ]]; then
-		wp core install --allow-root --url=anasr --title="$TITLE" --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASS --admin_email=$ADMIN_EMAIL
+		wp core install --allow-root --url=anasr.42.fr --title="$TITLE" --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASS --admin_email=$ADMIN_EMAIL
+#		Config required to allow media uploads 
+		chown -R www-data /var/www/html/wp-content/uploads
 	fi
 
 #	Create another user with a default subscriber role
-	wp user create --allow-root $NORMAL_USER $NORMAL_EMAIL --user_pass=$NORMAL_PASS
+	if [[ $(wp user get 2 --field=login --allow-root | grep $NORMAL_USER) -ne 0 ]]; then
+		wp user create --allow-root $NORMAL_USER $NORMAL_EMAIL --user_pass=$NORMAL_PASS
+	fi
+
+#	Change the theme
+	wp theme  activate twentytwentytwo --allow-root
+
+#	Bonus
+	if [[ $1 -eq "bonus" ]]; then
+#		Redis install and configuration
+		if [[ $(wp plugin is-installed redis-cache --allow-root) -ne 0 ]]; then
+			wp config set WP_REDIS_PATH --raw "__DIR__ . '/../redis.sock'" --allow-root
+			wp config set WP_REDIS_SCHEME "unix" --allow-root
+			wp plugin install redis-cache --activate --allow-root
+		fi
+	fi
 
 #	Update all plugins
 	wp plugin update --all --allow-root
