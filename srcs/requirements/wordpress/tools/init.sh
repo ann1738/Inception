c6c9f916ffdee17d2f07d1d5c3102f7e03d86423
ann <anasr@student.42abudhabi.ae>
2022-11-28 15:24:58 +0400

    Add Adminer to Wordpress

--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -19,7 +19,7 @@
 	fi
 
 #	Create another user with a default subscriber role
-	if [[ $(wp user get 2 --field=login --allow-root | grep $NORMAL_USER) -ne 0 ]]; then
+	if [[ $(wp user list --allow-root | grep $NORMAL_USER) != 0 ]]; then
 		wp user create --allow-root $NORMAL_USER $NORMAL_EMAIL --user_pass=$NORMAL_PASS
 	fi
 
@@ -27,14 +27,19 @@
 	wp theme  activate twentytwentytwo --allow-root
 
 #	Bonus
-	if [[ $1 -eq "bonus" ]]; then
+	# if [[ $1 == "bonus" ]]; then
 #		Redis install and configuration
-		if [[ $(wp plugin is-installed redis-cache --allow-root) -ne 0 ]]; then
-			wp config set WP_REDIS_PATH --raw "__DIR__ . '/../redis.sock'" --allow-root
-			wp config set WP_REDIS_SCHEME "unix" --allow-root
-			wp plugin install redis-cache --activate --allow-root
+		# if [[ $(wp plugin is-installed redis-cache --allow-root) != 0 ]]; then
+			# wp config set WP_REDIS_PATH --raw "__DIR__ . '/../redis.sock'" --allow-root
+			# wp config set WP_REDIS_SCHEME "unix" --allow-root
+			# wp plugin install redis-cache --activate --allow-root
+		# fi
+#		Move adminer.php to the root of website files
+		mkdir -p /var/www/html/adminer
+		if [[ $(ls /usr/share/adminer/ | grep adminer.php) != 0 ]]; then
+			mv /usr/share/adminer/adminer.php /var/www/html/adminer/
 		fi
-	fi
+	# fi
 
 #	Update all plugins
 	wp plugin update --all --allow-root
