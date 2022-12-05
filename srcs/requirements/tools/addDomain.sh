59854b5984e0690bb48f4175e4924a5ebce2146c
ann <anasr@student.42abudhabi.ae>
2022-12-05 14:06:52 +0400

    Adjust the script to add the proper domain

--- srcs/requirements/tools/addDomain.sh
+++ srcs/requirements/tools/addDomain.sh
@@ -2,8 +2,8 @@
 DOMAIN="anasr.42.fr"
 
 #Add domain to /etc/hosts if it doesn't exist
-cat /etc/hosts | grep "127.0.0.1" &> /dev/null && cat /etc/hosts | grep $DOMAIN &> /dev/null
+sudo cat /etc/hosts | grep "127.0.0.1" &> /dev/null && sudo cat /etc/hosts | grep $DOMAIN &> /dev/null
 if [[ $? -ne 0 ]]; then
-	echo -e "127.0.0.1\t$DOMAIN" >> /etc/hosts
+	echo -e "127.0.0.1\t$DOMAIN" | sudo tee --append /etc/hosts
 	echo "Added domain '$DOMAIN'"
 fi
