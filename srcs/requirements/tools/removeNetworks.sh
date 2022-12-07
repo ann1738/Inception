d8db9176350e102228fbc7b37d5b785b2c91c027
ann <anasr@student.42abudhabi.ae>
2022-12-07 12:14:42 +0400

    Adjust script to clean the docker networks to add bonus network

--- srcs/requirements/tools/removeNetworks.sh
+++ srcs/requirements/tools/removeNetworks.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 FRONTEND_NETWORK="srcs_duck"
 BACKEND_NETWORK="srcs_turtle"
+BONUS_NETWORK="srcs_bonus"
 
 docker network ls | grep $FRONTEND_NETWORK &> /dev/null
 if [[ $? -eq 0 ]]; then
@@ -12,4 +13,10 @@ docker network ls | grep $BACKEND_NETWORK &> /dev/null
 if [[ $? -eq 0 ]]; then
 	docker network rm $BACKEND_NETWORK &> /dev/null
 	echo "Removed network '$BACKEND_NETWORK'"
+fi
+
+docker network ls | grep $BONUS_NETWORK &> /dev/null
+if [[ $? -eq 0 ]]; then
+	docker network rm $BONUS_NETWORK &> /dev/null
+	echo "Removed network '$BONUS_NETWORK'"
 fi
